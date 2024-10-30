db314a30733e9898d52b13c7430ed3f3c99ad940
EduardGhazaryan <137617257+EduardGhazaryan@users.noreply.github.com>
2024-10-30 18:09:18 +0400

    fisrt commit

--- src/Redux/Slices/CurrentWeather/CurrentWeatherThunk.js
+++ src/Redux/Slices/CurrentWeather/CurrentWeatherThunk.js
@@ -5,7 +5,7 @@ export const getCuurentWeather = createAsyncThunk(
     "getCuurentWeather",
     async function ({city},{ rejectWithValue }){
         try {
-            const data = await instance.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=bb00b16d42720ba6515121bdbf548c03`)
+            const data = await instance.get(`/data/2.5/forecast?q=${city}&appid=bb00b16d42720ba6515121bdbf548c03`)
             return data.data
             
         } catch (error) {
